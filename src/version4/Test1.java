3539aa6bd5d43d4000a7e14514d511b2e41169c3
gaoyixue <3300978211@qq.com>
2017-10-20 17:28:48 +0800

    commit-checkstyle

--- src/version4/Test1.java
+++ src/version4/Test1.java
@@ -9,38 +9,37 @@ import javax.swing.JFrame;
 
 public class Test1 {
 
-	public static void main(String[] args)throws IOException  {
-		EventQueue.invokeLater(new Runnable(){  //时间分派线程
-			public void run()
-			{
-				SimpleFrame frame=new SimpleFrame();
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //关闭
-				frame.setVisible(true); //显示框架
-			}
-		});
-		
-		//Graph g = new Graph();
-	    //g.createGraph("D://test2.txt");
-		//g.showDirectedGraph();
-		//System.out.println(g.queryBridgeWords("strange", "worlds"));
-		//System.out.println(g.queryBridgeWords("strane", "worlds"));
-		//System.out.println(g.queryBridgeWords("new", "and"));
-		//System.out.println(g.queryBridgeWords("seek", "to"));
-	//	System.out.println(g.randomWalk());
-	//	System.out.println(g.randomWalk());
-	//	System.out.println(g.randomWalk());
-		//System.out.println(Arrays.toString(g.calcShortesePath("to", null)));
-	//	System.out.println(Arrays.toString(g.calcShortesePath("to","worlds")));
-		
-		//Scanner in = new Scanner(System.in);
-		//System.out.println("Input:");
-		//String s = in.nextLine();
-		//System.out.println(g.generateNewText(s));
-		
-		//remote test
-		//To@explore strange ne1w worlds,
-		//To seek out new life and new civilizations
-		
-		//git test2
-	}
+  public static void main(String[] args) throws IOException {
+    EventQueue.invokeLater(new Runnable() { // 时间分派线程
+      public void run() {
+        SimpleFrame frame = new SimpleFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // 关闭
+        frame.setVisible(true); // 显示框架
+      }
+    });
+
+    // Graph g = new Graph();
+    // g.createGraph("D://test2.txt");
+    // g.showDirectedGraph();
+    // System.out.println(g.queryBridgeWords("strange", "worlds"));
+    // System.out.println(g.queryBridgeWords("strane", "worlds"));
+    // System.out.println(g.queryBridgeWords("new", "and"));
+    // System.out.println(g.queryBridgeWords("seek", "to"));
+    // System.out.println(g.randomWalk());
+    // System.out.println(g.randomWalk());
+    // System.out.println(g.randomWalk());
+    // System.out.println(Arrays.toString(g.calcShortesePath("to", null)));
+    // System.out.println(Arrays.toString(g.calcShortesePath("to","worlds")));
+
+    // Scanner in = new Scanner(System.in);
+    // System.out.println("Input:");
+    // String s = in.nextLine();
+    // System.out.println(g.generateNewText(s));
+
+    // remote test
+    // To@explore strange ne1w worlds,
+    // To seek out new life and new civilizations
+
+    // git test2
+  }
 }
